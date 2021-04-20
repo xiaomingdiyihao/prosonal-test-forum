dcbdda39ae429743e8ca15f967ee43ff594aaba3
xiaomingdiyihao <18315165574@163.om>
2021-04-20 16:42:41 +0800

    feat:test add

--- src/test/java/forum/perosonalforum/PerosonalforumApplicationTests.java
+++ src/test/java/forum/perosonalforum/PerosonalforumApplicationTests.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
 @Slf4j
 @SpringBootTest
 class PerosonalforumApplicationTests {
@@ -11,6 +13,9 @@ class PerosonalforumApplicationTests {
     @Test
     void contextLoads() {
         log.info("this is test");
+        BigDecimal bigDecimal = new BigDecimal("100");
+        log.info(String.valueOf(bigDecimal.multiply(new BigDecimal("20"))));
+
     }
 
 }
