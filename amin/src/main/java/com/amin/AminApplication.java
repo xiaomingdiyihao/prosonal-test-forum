4236b12f4ea2a046bb8efa4e65eee8dd01128abf
xiaomingdiyihao <18315165574@163.om>
2021-06-16 15:38:53 +0800

    feat:test

---
+++ amin/src/main/java/com/amin/AminApplication.java
@@ -0,0 +1,13 @@
+package com.amin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AminApplication.class, args);
+    }
+
+}
